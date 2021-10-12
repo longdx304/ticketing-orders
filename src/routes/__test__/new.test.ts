e2a1309e7bbca60b5dcebe37ca38ad73dcc5a211
Long Do <longdo@dobtech.vn>
2021-10-13 00:13:20 +0700

    skaffold project and add basic new routes

---
+++ src/routes/__test__/new.test.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = global.createId();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId })
+    .expect(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+  // create & save ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  // create & save order with associated ticket
+  const order = Order.build({
+    ticket,
+    userId: 'asdas',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  // make the request and expect 400
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it('reserves a ticket', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  // request and expect 201
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
---
+++ src/routes/new.ts
@@ -0,0 +1,58 @@
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@ldxtickets/common';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+
+import { Order } from '../models/order';
+import { Ticket } from '../models/ticket';
+
+const router = express.Router();
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [body('ticketId').not().isEmpty().withMessage('Ticket Id must be provide')],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // find ticket user is trying to order in db
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // make sure ticket is not reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // calculate expiration
+    const expiration = new Date();
+    expiration.setSeconds(
+      expiration.getSeconds() +
+        Number(process.env.RESERVATION_EXPIRATION_WINDOW_SECONDS)
+    );
+
+    // build the order and save to db
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+
+    // publish event saying order was created
+
+    res.status(201).send(order);
+  }
+);
+
+export { router as newOrderRouter };
